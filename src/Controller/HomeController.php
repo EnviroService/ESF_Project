cd3b237b116026ace1cb1b6017bf09abb838d037
Doriane Tossou <dorianetossou@yahoo.fr>
2020-03-26 11:57:24 +0100

    header and a debut of contact

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+
+}
---
+++ src/Form/ContactType.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Form;
+
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+       $builder
+           ->add('society', TextType::class)
+           ->add('firstname', TextType::class)
+           ->add('lastname', TextType::class)
+           ->add('siret', TextType::class)
+           ->add('mail', EmailType::class)
+           ->add('num_tva', TextType::class)
+           ->add('phone_number', TextType::class)
+           ->add('message', TextareaType::class)
+           ->add('kbis', TextType::class)
+           ->add('cni', TextType::class)
+       ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+
+}
