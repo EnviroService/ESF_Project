0757a5e121cd009d0d8bba6ff036ac3d5c12697d
Jean-Roch Masson <jyaire@gmail.com>
2020-03-30 11:37:31 +0200

    First page for admin

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig');
+    }
+
+    /**
+     * @Route("/infos", name="infos")
+     */
+    public function infos()
+    {
+        return $this->render('home/infos.html.twig');
+    }
+}
+
