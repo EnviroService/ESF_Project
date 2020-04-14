874f4d9aee56c3cf798c03be81951ec64f53b802
Jean-Roch Masson <jyaire@gmail.com>
2020-04-14 14:13:29 +0200

    Install js for bootstrap, needed for carrousel

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,5 +11,11 @@ import '../scss/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
+const $ = require('jquery');
+require('bootstrap');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
